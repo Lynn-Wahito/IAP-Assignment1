181a54753e8ec0c69049278e603f521206464131
Masmnoma <alvinmaseno@gmail.com>
2023-09-22 21:38:46 +0300

    I have created the table connect to database and add data

---
+++ blog_app/listusers/table.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>List Users</title>
+  <style>
+    table {
+      border-collapse: collapse;
+      width: 100%;
+    }
+    
+    th, td {
+      padding: 8px;
+      text-align: left;
+      border-bottom: 1px solid #ddd;
+    }
+  </style>
+</head>
+<body>
+  <h1>List Users</h1>
+  <table>
+    <tr>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>User Name</th>
+      <th>Email</th>
+      <th>Role<th>
+
+    </tr>
+    <tr>
+      <!-- <td><?php  ?></td> -->
+      <!-- <td><?php  ?></td> -->
+      <!-- <td><?php  ?></td> -->
+      <!-- <td><?php  ?></td> -->
+      <!-- <td><?php  ?></td> -->
+    
+      
+    </tr>
+
+  </table>
+</body>
+</html>
